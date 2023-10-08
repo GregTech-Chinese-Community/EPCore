a165aeba65fbcbd2157fa83cc2db319d68593686
Oganesson897 <101081378+Darknight123MC@users.noreply.github.com>
2023-10-08 09:14:03 +0900

    Initial Commit

---
+++ src/main/java/cn/gtcommunity/epimorphism/Epimorphism.java
@@ -0,0 +1,12 @@
+package cn.gtcommunity.epimorphism;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = "epimorphism",
+        name = "Epimorphism",
+        acceptedMinecraftVersions = "[1.12.2,1.13)",
+        version = "0.0.1-beta"
+)
+public class Epimorphism {
+}
