2c3203c9ad2dbe2a9218b4da33462df7f751bfb2
qwer523 <xtzcy76@gmail.com>
2023-10-30 15:44:42 +0800

    A Little

--- src/main/java/cn/gtcommunity/epimorphism/loaders/formula/FormulaManager.java
+++ src/main/java/cn/gtcommunity/epimorphism/loaders/formula/FormulaManager.java
@@ -3,5 +3,6 @@ package cn.gtcommunity.epimorphism.loaders.formula;
 public class FormulaManager {
     public static void init() {
         BufferFormula.addBuffer();
+        CatalystFormula.addCatalyst();
     }
 }
