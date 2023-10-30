90bbaf4e3e8804eee1c5a973e41e9943a587c7b8
Gate Guardian <xtzcy76040108@163.com>
2023-10-30 07:42:50 +0000

    Little

---
+++ src/main/java/cn/gtcommunity/epimorphism/api/capability/EPDataCode.java
@@ -0,0 +1,13 @@
+package cn.gtcommunity.epimorphism.api.capability;
+
+public class EPDataCode {
+
+    //  Custom DataCode
+    //TODO 替换可读性更高的命名
+    public static final int EP_CHANNEL_1 = 9984;
+    public static final int EP_CHANNEL_2 = 9985;
+    public static final int EP_CHANNEL_3 = 9986;
+    public static final int EP_CHANNEL_4 = 9987;
+
+    private EPDataCode() {/**/}
+}
