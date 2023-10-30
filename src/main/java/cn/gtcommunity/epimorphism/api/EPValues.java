b690ea1347db36e0ac725eaa9258ef78a6e73a90
qwer523 <xtzcy76@gmail.com>
2023-10-30 15:41:37 +0800

    A Little

--- src/main/java/cn/gtcommunity/epimorphism/api/EPValues.java
+++ src/main/java/cn/gtcommunity/epimorphism/api/EPValues.java
@@ -2,10 +2,4 @@ package cn.gtcommunity.epimorphism.api;
 
 public class EPValues {
     public static final int OP = 101;
-
-    //  Custom Channel
-    public static final int EP_CHANNEL_1 = 9984;
-    public static final int EP_CHANNEL_2 = 9985;
-    public static final int EP_CHANNEL_3 = 9986;
-    public static final int EP_CHANNEL_4 = 9987;
 }
