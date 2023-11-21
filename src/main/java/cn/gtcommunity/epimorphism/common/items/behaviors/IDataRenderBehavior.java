83fc71dc889d5db19c0243caa8e226a0a44787f9
Gate Guardian <xtzcy76040108@163.com>
2023-11-21 01:33:23 +0000

    渲染更新

---
+++ src/main/java/cn/gtcommunity/epimorphism/common/items/behaviors/IDataRenderBehavior.java
@@ -0,0 +1,7 @@
+package cn.gtcommunity.epimorphism.common.items.behaviors;
+
+import cn.gtcommunity.epimorphism.api.items.metaitem.stats.IItemRendererManager;
+
+public interface IDataRenderBehavior extends IItemRendererManager {
+
+}
